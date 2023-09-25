979b96a53a59ef25c36d421c8dca141271972e66
K-Sricharan <sricharan.karingula@gmail.com>
2023-09-25 23:25:16 +0530

    Your commit message here

--- clean_data.py
+++ clean_data.py
@@ -1,8 +1,26 @@
 import logging
 import pandas as pd
 from zenml import step
-
+from src.data_cleanin import DataCleaning, DataDivideStrategy, DataPreprocessStrategy
+from typing_extensions import Annotated
+from typing import Tuple
 
 @step
-def clean_df(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+def clean_df(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
+    try:
+        process_strategy = DataPreprocessStrategy()
+        data_cleaning = DataCleaning(df,process_strategy)
+        processed_data =data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train,X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data Cleaningcompleted")
+    except Exception as e:
+        logging.error ("Error in cleaning data: {}".format(e))
+        raise e
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,4 +1,6 @@
-from pipelines.training_pipeline import train_pipeline
+from training_pipeline import train_pipeline
 
 if __name__== "__main__":
     train_pipeline(data_path = "E:\Antern\Pipeline_Project_1\Steps\olist_customers_dataset.csv")
+
+
